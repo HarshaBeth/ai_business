d80262746da4b4a3ae8006d073c8c3c234dbefc0
HarshaBeth <harshabbeth@gmail.com>
2024-08-07 13:59:36 +0400

    Compiler screen responsive

--- src/components/Compiler.jsx
+++ src/components/Compiler.jsx
@@ -4,12 +4,12 @@ const Compiler = () => {
   return (
     <div className='h-screen w-full flex flex-col justify-center gap-8 bg-gray-100'>
 
-        <div className='text-3xl font-bold pl-20 w-full flex flex-col'>
-            <span>Embedded Sandbox: Code your <span className='text-green-500 italic'>architecture</span></span>
+        <div className='text-3xl font-bold pl-20 max-md:pl-11 w-full flex flex-col max-lg:text-xl max-sm:w-[60%]'>
+            <span>Embedded Sandbox: <span className='max-sm:bg-green-500 max-sm:px-0.5'> Code your <span className='text-green-500 max-sm:text-white italic'>architecture</span></span></span>
             <span className='text-sm font-semibold italic'>(Open Sandbox to view diagram)</span>
         </div>
 
-        <div className='w-full px-20'>
+        <div className='w-full px-20 max-md:px-11'>
             <iframe src="https://codesandbox.io/embed/2jlgpj?view=editor+%2B+preview&module=%2Fsrc%2FApp.js"
                 style={{width:"100%", height: "500px", border:"0", borderRadius: "4px", overflow:"hidden"}}
                 title="musing-darkness"
